f1463ba5db0078eca525d1276566aba2ba04d56e
Daniel1024-maker <72279586+Daniel1024-maker@users.noreply.github.com>
2025-07-22 15:03:06 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 function AppRoutes(){
     return(
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="https://daniel1024-maker.github.io/TD-2-Web/home" element={<Home />}></Route>
                 <Route path="https://daniel1024-maker.github.io/TD-2-Web" element={<Home />}></Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 export default AppRoutes;
